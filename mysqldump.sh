e88461c1ac52b6e7cf36aadfc621d0ba1aa9036c
Edwin Fajardo <ingedwinfajardo@hotmail.com>
2022-08-05 06:10:29 -0600

    Exclusión de triggers en la volcado de datos de mysqldump

--- mysqldump.sh
+++ mysqldump.sh
@@ -34,7 +34,7 @@ for folder in "$@"; do
 		mysqldump -u root -pldi14517 -d --skip-dump-date ${databases[$folder]} | sed 's/ AUTO_INCREMENT=[0-9]*//g' | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' > $temp_dir/$folder/db_structure.sql
 
 		# Volcado de los valores de todas las tablas que inician con app_*
-		mysqldump -u root -pldi14517 -t --skip-dump-date ${databases[$folder]} $(mysql -u root -pldi14517 -D ${databases[$folder]} -Bse "SHOW TABLES LIKE 'app_%'") > $temp_dir/$folder/initial_data.sql
+		mysqldump -u root -pldi14517 -t --skip-dump-date --skip-triggers ${databases[$folder]} $(mysql -u root -pldi14517 -D ${databases[$folder]} -Bse "SHOW TABLES LIKE 'app_%'") > $temp_dir/$folder/initial_data.sql
 
 		# Se comprueba que exista un archivo previo de la estructura, y que es diferente. Si el archivo no existe, se crea.
 		rsync -c --info=NAME1 "$temp_dir/$folder/db_structure.sql" ./db_structure.sql
