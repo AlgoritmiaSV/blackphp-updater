c705f921c944c3d7c491b5a441d074db791015a1
Edwin Fajardo <contacto@edwinfajardo.com>
2024-11-19 06:50:47 -0600

    Cambio de nombre de tabla app_payments a app_payment_types

--- language.sh
+++ language.sh
@@ -70,11 +70,11 @@ grep -nrw "$project_path/controllers/" -Ee '_\([^\)]+\)'  | sed -E 's/\)/\)\n/g'
 # -> Nombre singular y plural de los elementos
 mysql --skip-column-names -h $db_host -u $db_user -p$db_password $database -e "SELECT module_name FROM ${db_prefix}app_modules WHERE status = 1 UNION ALL SELECT method_name FROM ${db_prefix}app_methods WHERE status = 1 UNION ALL SELECT theme_name FROM ${db_prefix}app_themes UNION ALL SELECT element_name FROM ${db_prefix}app_elements UNION ALL SELECT singular_name FROM ${db_prefix}app_elements UNION ALL SELECT locale_name FROM ${db_prefix}app_locales UNION ALL SELECT option_description FROM ${db_prefix}app_options" >> required.txt
 
-# Evaluando si existe la tabla app_payments
-app_payments=`mysql --skip-column-names -h $db_host -u $db_user -p$db_password information_schema -e "SELECT 1 FROM TABLES WHERE TABLE_SCHEMA = '$database' AND TABLE_NAME = '${db_prefix}app_payments'"`
+# Evaluando si existe la tabla app_payment_types
+app_payments=`mysql --skip-column-names -h $db_host -u $db_user -p$db_password information_schema -e "SELECT 1 FROM TABLES WHERE TABLE_SCHEMA = '$database' AND TABLE_NAME = '${db_prefix}app_payment_types'"`
 if [ "$app_payments" = "1" ]; then
 	# Extrayendo las formas de pago de la base de datos
-	mysql --skip-column-names -h $db_host -u $db_user -p$db_password $database -e "SELECT CONCAT('payments', ptype_name) FROM ${db_prefix}app_payments" >> required.txt
+	mysql --skip-column-names -h $db_host -u $db_user -p$db_password $database -e "SELECT CONCAT('payments', type_name) FROM ${db_prefix}app_payment_types" >> required.txt
 fi
 
 # Evaluando si existe la tabla app_documents
